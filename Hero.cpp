6033b9bba92626ccd4310e19b7f650d768ae7070
Артем Козьмин <artemkozmin@MacBook-Pro-Artem-2.local>
2018-12-20 14:47:09 +0700

    Update Hero.cpp

--- Hero.cpp
+++ Hero.cpp
@@ -11,6 +11,7 @@ Hero::Hero(std::string Name, Map Field, int x, int y, std::string Player){
 	buff_army = std::vector<float>();
 	buff_resource = std::vector<float>();
 }
+Hero::Hero(){};
 int Hero::Get_x() {
 	return pos.Get_x();
 }
